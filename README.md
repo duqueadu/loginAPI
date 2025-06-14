669c2069edff490b4ad6025f764fe7b67821375b
duqueadu <duqueadu@gmail.com>
2025-06-13 21:42:52 -0500

    login ejemplo

--- README.md
+++ README.md
@@ -13,8 +13,24 @@
                                 {usuario = prueba2@prueba.com,clve = 12345678}
                                 {usuario = prueba1@prueba.com,clave =12345678}
 
+                                
+
                             1. Registrar usuario en /api/authorization/CrearUsuario
+                            Ejemplo 
+                            {
+                                nombre: Juan Pérez,
+                                email: juan.perez@example.com,
+                                contrasena: segura123,
+                                rol: 1,
+                                fechaCreacion: 2025-06-14T10:00:00.000Z
+                             }
                             2. Iniciar sesión en /api/authorization/login
+                            Ejemplo 
+                            {
+                                email: juan.perez@example.com,
+                                contrasena: segura123
+                             }
+                          
                             3. Introducir el token JWT recibido en el botón Authorize
                             4. Acceder a los endpoints protegidos
                             
--- src/main/java/com/prueba/loginAPI/infrastructure/config/SwaggerConfig.java
+++ src/main/java/com/prueba/loginAPI/infrastructure/config/SwaggerConfig.java
@@ -41,8 +41,21 @@ public class SwaggerConfig {
                                 {usuario = prueba1@prueba.com,clave =12345678}
 
                             ###  Instrucciones:
-                            1. Registrar usuario en /api/authorization/CrearUsuario
+                                 1. Registrar usuario en /api/authorization/CrearUsuario
+                            Ejemplo 
+                            {
+                                nombre: Juan Pérez,
+                                email: juan.perez@example.com,
+                                contrasena: segura123,
+                                rol: 1,
+                                fechaCreacion: 2025-06-14T10:00:00.000Z
+                             }
                             2. Iniciar sesión en /api/authorization/login
+                            Ejemplo 
+                            {
+                                email: juan.perez@example.com,
+                                contrasena: segura123
+                             }
                             3. Introducir el token JWT recibido en el botón Authorize
                             4. Acceder a los endpoints protegidos
                             
